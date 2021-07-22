995942367512c90d6955bc69fffb352a8ce0784a
catpea <corporate.miou@gmail.com>
2021-07-22 19:30:04 -0400

    New Release

--- test.js
+++ test.js
@@ -4,6 +4,5 @@ import {equal, deepEqual} from 'assert';
 import rsend from './index.js';
 
 const expected = true;
-// should be disabled for safety
-const actual = await rsend('test', 'earth:/tmp/rsend-test');
+// should be disabled for safety: const actual = await rsend('test', 'earth:/tmp/rsend-test');
 equal(actual, expected);
